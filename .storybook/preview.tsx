d3b9935d90a4d98870031c2a93998e64e5476b0a
Stephen N <sndegwa.n@outlook.com>
2025-03-20 16:55:49 +0300

    Added button storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import type { Preview } from "@storybook/react";
+import { ThemeProvider } from "../src/ui/styles/ThemeProvider";
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export default preview;
--- src/ui/components/atoms/Button/Buttons.stories.tsx
+++ src/ui/components/atoms/Button/Buttons.stories.tsx
@@ -4,13 +4,51 @@ import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
+  title: "Components/Atoms/Button",
+  argTypes: {
+    color: {
+      control: "select",
+      options: ["primary", "secondary", "gray"],
+    },
+    size: {
+      control: "select",
+      options: ["xs", "sm", "md", "lg", "xl"],
+    },
+    disabled: {
+      control: "boolean",
+    },
+  },
+  args: {
+    loading: false,
+    children: "Hello, World!",
+    variant: "solid",
+    size: "md",
+    disabled: false,
+    color: "primary",
+  },
+  parameters: {
+    controls: { exclude: /^variant|children*/ },
+  },
+};
+
+type Story = StoryObj<typeof Button>;
+
+export const SolidBtn: Story = {
+  args: {
+    variant: "solid",
+  },
+};
+
+export const OutlinedBtn: Story = {
+  args: {
+    variant: "outlined",
+  },
+};
+
+export const TextBtn: Story = {
+  args: {
+    variant: "text",
+  },
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>;
-
-export const Primary: Story = {
-  args: {
-    children: "Hello, World!",
-  },
-};
